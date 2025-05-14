78a9b2b12e3ec410f022f95fb1deca55c94275c9
Nathan Ranchin <nathan.ranchin@epfl.ch>
2025-05-14 14:58:49 +0200

    basic personal page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,15 +2,11 @@
 
 import { useEffect, useState } from "react";
 import { DatasetObject } from "@/lib/types";
-import { useCurrentAccount } from "@mysten/dapp-kit";
+import { getLockedDatasets } from "@/lib/actions";
 import { DatasetList } from "@/components/dataset-list";
-import { getLockedDatasets, getPersonalDatasets } from "@/lib/actions";
 
 export default function Home() {
   const [lockedDatasets, setLockedDatasets] = useState<DatasetObject[]>([]);
-  const [personalDatasets, setPersonalDatasets] = useState<DatasetObject[]>([]);
-
-  const currentAccount = useCurrentAccount();
 
   useEffect(() => {
     getLockedDatasets().then(datasets => {
@@ -18,34 +14,14 @@ export default function Home() {
     });
   }, []);
 
-  useEffect(() => {
-    if (currentAccount) {
-      getPersonalDatasets(currentAccount.address).then(datasets => {
-        setPersonalDatasets(datasets);
-      });
-    } else {
-      setPersonalDatasets([]);
-    }
-  }, [currentAccount]);
-
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">My Datasets</h1>
-      {currentAccount && personalDatasets.length === 0 && (
-        <p>No datasets found for your account.</p>
-      )}
-      {!currentAccount && (
-        <p>Please connect your wallet to see your datasets.</p>
-      )}
-      {personalDatasets.length > 0 && (
-        <DatasetList datasets={personalDatasets} />
-      )}
-      {lockedDatasets.length > 0 && (
-        <div className="mt-8">
-          <h2 className="text-2xl font-bold mb-4">Locked Datasets</h2>
+      <div className="mt-8">
+        <h2 className="text-2xl font-bold mb-4">Trending Datasets</h2>
+        {lockedDatasets.length > 0 && (
           <DatasetList datasets={lockedDatasets} />
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
--- app/src/app/user/[id]/page.tsx
+++ app/src/app/user/[id]/page.tsx
@@ -1,9 +1,28 @@
-import { use } from "react";
+"use client";
+
+import { DatasetObject } from "@/lib/types";
+import { use, useEffect, useState } from "react";
+import { getPersonalDatasets } from "@/lib/actions";
+import { DatasetList } from "@/components/dataset-list";
 
 export default function UserPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = use(params);
+  const [personalDatasets, setPersonalDatasets] = useState<DatasetObject[]>([]);
 
-  return <div>
-    <h1>User {id}</h1>
-  </div>;
+  useEffect(() => {
+    getPersonalDatasets(id).then(datasets => {
+      setPersonalDatasets(datasets);
+    });
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="mt-8">
+        <h2 className="text-2xl font-bold mb-4">Personal Datasets</h2>
+        {personalDatasets.length > 0 && (
+          <DatasetList datasets={personalDatasets} />
+        )}
+      </div>
+    </div>
+  );
 }
